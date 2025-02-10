dfb1f2a24013f8a47cab5bd847baeb44a724d5b8
HaiderAzeemBaramdat <haider.azeem@baramdatsol.com>
2025-02-10 18:02:01 +0500

    fix some issues

--- src/screens/audioPlayerPage/index.tsx
+++ src/screens/audioPlayerPage/index.tsx
@@ -36,9 +36,7 @@ const Index = ({ route }: { route: any }) => {
         >
           <Stack px={15}>
             <Typography type="caption">
-              Introduction about the practice session. Summarisation so they
-              understand what is it about. Make quick decision if they want to
-              play this.
+              {route?.params?.data?.description}
             </Typography>
             <AudioPlayer
               data={route?.params?.data}
